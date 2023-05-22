dcf606d767fdd5cc763a1e703ae800e425024186
Azeddine El makkaoui <ael-makk@e2r9p15.1337.ma>
2023-05-22 15:24:55 +0100

    add components

--- bank_modern_app/postcss.config.cjs
+++ bank_modern_app/postcss.config.cjs
@@ -1,6 +1,12 @@
-export default {
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+  tailwindcss: {},
+  autoprefixer: {},
   },
-}
+  }
